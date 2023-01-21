ae1a946fe9ab03f0aa2277c0abb8f98d465f0fe9
Erik Vodopivec Forsman <02linfor01@gmail.com>
2023-01-21 19:29:30 +0000

    changed bootstrap version

--- calculator/urls.py
+++ calculator/urls.py
@@ -5,4 +5,4 @@ from django.urls import path
 urlpatterns = [
     #path('', views.calc, name='calc'),
     path('', views.LoadHomePage.as_view())
-] 
+]
--- snappy_savings/settings.py
+++ snappy_savings/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'snappy_savings.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
+#DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
