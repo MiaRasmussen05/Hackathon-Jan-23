bd7ba17a1d1492f293c3ffed358825e323a0c6e8
Erik Vodopivec Forsman <02linfor01@gmail.com>
2023-01-21 22:13:12 +0000

    basic calculation working

--- calculator/views.py
+++ calculator/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, get_object_or_404, reverse
+from django.shortcuts import render, redirect, get_object_or_404, reverse
 from django.views import generic, View
 #from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from .models import CalcFunding
 from .forms import CalcFundingForm
+
+
 
 
 class LoadHomePage(View):
@@ -12,19 +14,28 @@ class LoadHomePage(View):
 
 
 def calc(request):
-    if request.method == 'POST':
-        form = CalcFundingForm(request.POST, request.FILES)
-        if form.is_valid():
-            calc_funding = form.save()
-    else:
-         form = CalcFundingForm()    
-    #extra = calc_funding.salary_post - calc_funding.expences
-
-    #if calc_funding.
-
+    form = CalcFundingForm()
     template = 'calculator/calc-funding.html'
     context = {
         'form': form,
     }
 
     return render(request, template, context)
+
+
+def run_calc(request):
+    """ Run calculation with given values """
+    if request.method == 'POST':
+        form = CalcFundingForm(request.POST, request.FILES)
+        if form.is_valid():
+            calc_funding = form.save()
+            extra = calc_funding.salary_post - calc_funding.expences
+            if calc_funding.savings > (calc_funding.salary_pre * 3):
+                print("success")
+            else:
+                print("error")
+            print("post")
+    else:
+        form = CalcFundingForm()
+
+    return redirect(reverse('calc'))
