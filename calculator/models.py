def9d6fada9178ceac5059c3ca2ea91bc1b8474e
Erik Vodopivec Forsman <02linfor01@gmail.com>
2023-01-22 00:41:13 +0000

    made it possible to choose how to fund purchase

--- calculator/models.py
+++ calculator/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
+CHOICES = (('SV', 'Savings'), ('SA', "Salary"))
+
 class CalcFunding(models.Model):
     cost = models.IntegerField()
     savings = models.IntegerField()
     salary_pre = models.IntegerField()
     salary_post = models.IntegerField()
     expences = models.IntegerField()
+    fund = models.CharField(max_length=200, choices=CHOICES, default="SV")
 
     class Meta:
         ordering = ['cost']
