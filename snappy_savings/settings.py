52e8143af926102cd4933fe31e16b7305df44ca2
JamesMartin1998 <jamesandrewmartin1998@hotmail.co.uk>
2023-01-22 19:13:27 +0000

    import dj_database_url

--- snappy_savings/settings.py
+++ snappy_savings/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-#import dj_database_url
+import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
